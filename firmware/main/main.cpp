3de5490caf72f71cef9b91005eded5e1c9e7ff9a
fredj hammami <contact.hammami.fredj@gmail.com>
2026-06-15 17:52:12 +0200

    remove hardcoded example wifi creds

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -1,9 +1,5 @@
 #include <hal/hal.h>
 #include <mooncake_log.h>
-#include <ssid_manager.h>
-
-#define WIFI_SSID "Livebox-BD4A"
-#define WIFI_PASSWORD "r2szcn7GdJ7ZzcnGKZ"
 
 extern "C" void app_main(void)
 {
@@ -11,8 +7,5 @@ extern "C" void app_main(void)
     mclog::set_time_format(mclog::time_format_unix_milliseconds);
 
     GetHAL().init();
-
-    SsidManager::GetInstance().AddSsid(WIFI_SSID, WIFI_PASSWORD);
-
     GetHAL().startXiaozhi();
 }
